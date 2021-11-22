2f3f882292c8a9562c7c2c60e93fde1560025060
adaam <adam.cws@gmail.com>
2021-11-22 16:20:37 +0800

    update 123

---
+++ modules/infra/vpc.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source           = "terraform-aws-modules/vpc/aws"
+  version          = "3.6.0"
+  name             = "testVPC"
+  cidr             = "10.0.0.0/16"
+  azs              = ["us-east-1a","us-east-1b"]
+  private_subnets  = [ "10.0.10.0/24", "10.0.11.0/24" ]
+  public_subnets   = [ "10.0.20.0/24", "10.0.21.0/24" ]
+
+  enable_nat_gateway = false
+
+  create_database_subnet_group = false
+  single_nat_gateway           = true
+  one_nat_gateway_per_az       = false
+  enable_dns_hostnames         = true
+}
