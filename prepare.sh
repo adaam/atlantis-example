277e8e27022cd9302d295d03fc6c879578efd40a
adaam <adam.cws@gmail.com>
2021-11-23 17:37:03 +0800

    prepare sh

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,8 @@
 
 echo "Exporting secrets"
 AWS_REGION=us-west-2 ./chamber-amd64 export test/terraform -f tfvars -o secrets.tfvars
-exit 222
+
+if [ $? -ne 0 ]; then 
+  echo 'error when export secrets'
+  exit 1
+fi
